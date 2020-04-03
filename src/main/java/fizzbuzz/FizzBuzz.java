15fb7e755ee528e311662e6806ac295ffa738a7c
krzwier <krzwier@gmail.com>
2020-04-03 17:59:20 -0500

    Pass test for input of 3.

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,9 @@ package fizzbuzz;
 public final class FizzBuzz {
     
 	public String getValue(int i) {
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
         return Integer.toString(i);
     }
     
