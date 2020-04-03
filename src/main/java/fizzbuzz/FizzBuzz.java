7e16bd585a05099e62e409984c82122575d310c5
krzwier <krzwier@gmail.com>
2020-04-03 18:07:38 -0500

    Pass test case for input 15.

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,9 @@ package fizzbuzz;
 public final class FizzBuzz {
     
 	public String getValue(int i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        } 
         if (i % 3 == 0) {
             return "Fizz";
         }
